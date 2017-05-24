c754ee85510ee47d8e7ea37fada01106700b7c77
Agustin <elmaco92@gmail.comm>
2017-05-24 20:25:22 -0300

    added linter

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,5 +1,6 @@
 'use strict';
 
+require('/daemons/mqtt-brain');
 const express = require('express');
 
 const router = express.Router();
@@ -7,6 +8,7 @@ const router = express.Router();
 /* GET home page. */
 router.get('/', (req, res) => {
   res.render('index', { title: 'Express' });
+  console.log('hola');
 });
 
 module.exports = router;
