bc3519550477c3e8b968ed3d395b19234861429e
Agustin Collazo Mayer <acollazomayer@gmail.com>
2017-12-06 19:49:49 -0300

    Update photo.js

--- app/brain/controllers/photo.js
+++ app/brain/controllers/photo.js
@@ -2,15 +2,18 @@
 
 const i2b = require('imageurl-base64');
 const miniponic = require('../../../config/miniponic.json');
+const axios = require('axios');
 
 function takePicture() {
   return new Promise((resolve, reject) => {
-    // resolve('photo');
     i2b(miniponic.PHOTO_CALL, (error, photo) => {
-      if (error) resolve(error);
-      resolve(photo.base64);
-    });
-  });
+      if (error) reject(error);
+
+      axios.post(url, { photo: photo.base64 })
+        .catch((err) => {
+          console.log(err.statusMessage);
+        });
+   });
 }
 
 module.exports = takePicture;
